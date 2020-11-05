dda66ae4c1900dbef79ab45bdc7b3adbb516a5c3
Aleksey Shundrik <shundrikaleksey@gmail.com>
2020-11-05 19:15:03 +0300

    rl-492/openapi-generator OpenapiGenerator: (#13)
    * rl-492/openapi-generator OpenapiGenerator:
    - add ability to run v5.0.0-beta openapi generator version
    - add ability to run update-models.sh without DOCS_DEFINITION_FOLDER environment
    
    * rl-492/openapi-generator OpenapiGenerator:
    - change openapi generator tag: v5.0.0-beta -> v5.0.0-beta2
    
    Co-authored-by: Aleksey Shundrik <al.shundrik@insspring.com>

--- update-models.sh
+++ update-models.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
 
-ENUM_MAPPINGS="TextFieldType=Integer,GraphicFieldType=Integer,Scenario=String,DocumentFormat=Integer,\
+DOCS_DEFINITION_FOLDER="${PWD}/../DocumentReader-web-openapi" \
+\
+&& ENUM_MAPPINGS="TextFieldType=Integer,GraphicFieldType=Integer,Scenario=String,DocumentFormat=Integer,\
 Light=Integer,Result=Integer,VerificationResult=Integer,RfidLocation=Integer,\
 DocumentTypeRecognitionResult=Integer,ProcessingStatus=Integer,Source=String,CheckResult=Integer,\
 LCID=Integer,DocumentType=Integer" \
 \
 && docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "$DOCS_DEFINITION_FOLDER:/definitions" \
-openapitools/openapi-generator-cli generate \
+openapitools/openapi-generator-cli:v5.0.0-beta2 generate \
 -i /definitions/index.yml -g java -o /client/client \
 -c /client/java-generator-config.json -t /client/client/generator-templates/ \
 \
 && docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "${DOCS_DEFINITION_FOLDER}:/definitions" \
-openapitools/openapi-generator-cli generate \
+openapitools/openapi-generator-cli:v5.0.0-beta2 generate \
 -i /client/docs/openapi/index.yml -g java -o /client/clients/java/client \
 -i /definitions/index.yml -g java -o /client/client \
 -c /client/java-generator-config.json -t /client/client/generator-templates/ \
 --import-mappings $ENUM_MAPPINGS \
 \
 && docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "${DOCS_DEFINITION_FOLDER}:/definitions" \
-openapitools/openapi-generator-cli generate \
+openapitools/openapi-generator-cli:v5.0.0-beta2 generate \
 -i /definitions/index.yml -g java -o /client/client \
 -c /client/java-generator-config.json -t /client/client/generator-templates/ \
 --import-mappings $ENUM_MAPPINGS,TextField=com.regula.documentreader.webclient.model.ext.TextField,\
 ImagesField=com.regula.documentreader.webclient.model.ext.ImagesField \
 \
 && docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "${DOCS_DEFINITION_FOLDER}:/definitions" \
-openapitools/openapi-generator-cli generate \
+openapitools/openapi-generator-cli:v5.0.0-beta2 generate \
 -i /definitions/index.yml -g java -o /client/client \
 -c /client/java-generator-config.json -t /client/client/generator-templates/ \
 --import-mappings $ENUM_MAPPINGS,Text=com.regula.documentreader.webclient.model.ext.Text,\
