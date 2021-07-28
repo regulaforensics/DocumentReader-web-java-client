2e2d0e710231cf4dbe73c5b0698e927738f47e01
Hleb Albau <hleb.albau@gmail.com>
2021-07-28 15:45:55 +0300

    Update Main.java

--- example/src/main/java/com/regula/documentreader/webclient/example/Main.java
+++ example/src/main/java/com/regula/documentreader/webclient/example/Main.java
@@ -53,6 +53,7 @@ public class Main {
                         // actual results
                         Result.STATUS, Result.AUTHENTICITY, Result.TEXT, Result.IMAGES,
                         Result.DOCUMENT_TYPE, Result.DOCUMENT_TYPE_CANDIDATES, Result.IMAGE_QUALITY,
+                        Result.DOCUMENT_POSITION,
                         // legacy results
                         Result.MRZ_TEXT, Result.VISUAL_TEXT, Result.BARCODE_TEXT, Result.RFID_TEXT,
                         Result.VISUAL_GRAPHICS, Result.BARCODE_GRAPHICS, Result.RFID_GRAPHICS,
