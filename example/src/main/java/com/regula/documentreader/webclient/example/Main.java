32e5932648b1e74a7a0c0a51b659bf708dffab88
Hleb Albau <hleb.albau@gmail.com>
2021-08-16 13:33:23 +0300

    example of deserializing json response

--- example/src/main/java/com/regula/documentreader/webclient/example/Main.java
+++ example/src/main/java/com/regula/documentreader/webclient/example/Main.java
@@ -1,11 +1,14 @@
 package com.regula.documentreader.webclient.example;
 
 
+import com.google.gson.reflect.TypeToken;
 import com.regula.documentreader.webclient.ApiException;
+import com.regula.documentreader.webclient.JSON;
 import com.regula.documentreader.webclient.api.DocumentReaderApi;
 import com.regula.documentreader.webclient.model.CheckResult;
 import com.regula.documentreader.webclient.model.LexicalAnalysisResult;
 import com.regula.documentreader.webclient.model.Light;
+import com.regula.documentreader.webclient.model.ProcessResponse;
 import com.regula.documentreader.webclient.model.Result;
 import com.regula.documentreader.webclient.model.Scenario;
 import com.regula.documentreader.webclient.model.SecurityFeatureType;
@@ -17,6 +20,7 @@ import com.regula.documentreader.webclient.model.ext.RecognitionResponse;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -32,47 +36,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ApiException {
 
-        var apiBaseUrl = System.getenv(API_BASE_PATH);
-        if (apiBaseUrl == null) {
-            apiBaseUrl = "https://api.regulaforensics.com";
-        }
-        var licenseFromEnv = System.getenv(TEST_LICENSE); // optional, used here only for smoke test purposes
-        var licenseFromFile = readFile("regula.license");
+        var responseAsBytes = readFile("response.json");
+        var serde = new JSON();
+        Type responseType = new TypeToken<ProcessResponse>() {
+        }.getType();
+        ProcessResponse originalResponse = serde.deserialize(new String(responseAsBytes), responseType);
+        RecognitionResponse response = new RecognitionResponse(originalResponse);
 
-        byte[] whitePage0 = readFile("WHITE.jpg");
-        byte[] irPage0 = readFile("IR.jpg");
-        byte[] uvPage0 = readFile("UV.jpg");
 
-        var whitePageRequestImage = new ProcessRequestImage(whitePage0, Light.WHITE, 0);
-        var irPageRequestImage = new ProcessRequestImage(irPage0, Light.IR, 0);
-        var uvPageRequestImage = new ProcessRequestImage(uvPage0, Light.UV, 0);
-
-        var requestParams = new RecognitionParams()
-                .withScenario(Scenario.FULL_AUTH)
-                .withResultTypeOutput(
-                        // actual results
-                        Result.STATUS, Result.AUTHENTICITY, Result.TEXT, Result.IMAGES,
-                        Result.DOCUMENT_TYPE, Result.DOCUMENT_TYPE_CANDIDATES, Result.IMAGE_QUALITY,
-                        Result.DOCUMENT_POSITION,
-                        // legacy results
-                        Result.MRZ_TEXT, Result.VISUAL_TEXT, Result.BARCODE_TEXT, Result.RFID_TEXT,
-                        Result.VISUAL_GRAPHICS, Result.BARCODE_GRAPHICS, Result.RFID_GRAPHICS,
-                        Result.LEXICAL_ANALYSIS
-                );
-
-        RecognitionRequest request = new RecognitionRequest(
-                requestParams, List.of(whitePageRequestImage, irPageRequestImage, uvPageRequestImage)
-        );
-
-        var api = new DocumentReaderApi(apiBaseUrl);
-        if (licenseFromEnv != null) api.setLicense(licenseFromEnv);
-        if (licenseFromFile != null) api.setLicense(licenseFromFile);
-
-        var info = api.ping();
         System.out.println("-----------------------------------------------------------------");
-        System.out.format("Web API version %s%n", info.getVersion());
-
-        RecognitionResponse response = api.process(request);
 
         var status = response.status();
         var docOverallStatus = status.getOverallStatus() == CheckResult.OK ? "valid" : "not valid";
@@ -87,14 +59,6 @@ public class Main {
         var docNumberMrzValidity = docNumberField.sourceValidity(Source.MRZ);
         var docNumberMrzVisualMatching = docNumberField.crossSourceComparison(Source.MRZ, Source.VISUAL);
 
-        var docAuthenticity = response.authenticity();
-
-        var docIRB900 = docAuthenticity.irB900Checks();
-        var docIRB900Blank = docIRB900.checksByElement(SecurityFeatureType.BLANK);
-
-        var docImagePattern = docAuthenticity.imagePatternChecks();
-        var docImagePatternBlank = docImagePattern.checksByElement(SecurityFeatureType.BLANK);
-
         System.out.println("-----------------------------------------------------------------");
         System.out.format("           Document Overall Status: %s%n", docOverallStatus);
         System.out.format("            Document Number Visual: %s%n", docNumberVisual);
@@ -104,14 +68,6 @@ public class Main {
         System.out.format("      MRZ-Visual values comparison: %s%n", docNumberMrzVisualMatching);
         System.out.format("-----------------------------------------------------------------");
 
-        var documentImage = response.images().getField(DOCUMENT_FRONT).getValue();
-        var portraitField = response.images().getField(PORTRAIT);
-        var portraitFromVisual = portraitField.getValue(Source.VISUAL);
-        saveFile("document-image.jpg", documentImage);
-        saveFile("portrait.jpg", portraitFromVisual);
-
-        // how to get low lvl individual results
-        LexicalAnalysisResult lexResult = response.resultByType(Result.LEXICAL_ANALYSIS);
         System.exit(0);
     }
 
