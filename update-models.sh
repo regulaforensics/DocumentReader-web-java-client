1692ce679e0ad9e4185d9eb8fe3a009d306ccff1
Artem Tsybulko <tsybulko.artem@gmail.com>
2024-10-22 12:18:04 +0300

    Update models, add new enums

--- update-models.sh
+++ update-models.sh
@@ -2,12 +2,15 @@
 
 DOCS_DEFINITION_FOLDER="${PWD}/../DocumentReader-web-openapi" \
 \
-&& ENUM_MAPPINGS="MeasureSystem=Integer,TextFieldType=Integer,GraphicFieldType=Integer,Scenario=String,DocumentFormat=Integer,\
-Light=Integer,Result=Integer,VerificationResult=Integer,RfidLocation=Integer,\
-DocumentTypeRecognitionResult=Integer,ProcessingStatus=Integer,Source=String,CheckResult=Integer,\
-LCID=Integer,DocumentType=Integer,CheckDiagnose=Integer,Critical=Integer,AuthenticityResultType=Integer,\
-SecurityFeatureType=Integer,Visibility=Integer,ImageQualityCheckType=Integer,\
-LogLevel=String,MRZFormat=String,TextPostProcessing=Integer" \
+&& ENUM_MAPPINGS="CheckDiagnose=Integer,Critical=Integer,AuthenticityResultType=Integer,Visibility=Integer,Source=String,\
+CheckResult=Integer,Scenario=String,RfidLocation=Integer,Light=Integer,BarcodeType=Integer,DocumentType=Integer,\
+GraphicFieldType=Integer,LCID=Integer,ParsingErrorCodes=Integer,ParsingNotificationCodes=Integer,RfidDataFileType=Integer,\
+RfidDataGroupTypeTag=Integer,RFIDErrorCodes=Integer,RFIDPKDResourceType=Integer,SecurityFeatureType=Integer,\
+TextFieldType=Integer,MRZFormat=String,TextPostProcessing=Integer,mrzDetectModeEnum=Integer,MeasureSystem=Integer,\
+LogLevel=String,ProcessingStatus=Integer,BarCodeModuleType=Integer,DocumentTypeRecognitionResult=Integer,\
+DocumentFormat=Integer,ImageQualityCheckType=Integer,VerificationResult=Integer,RfidApplicationType=Integer,\
+RfidAccessControlProcedureType=Integer,RfidType=Integer,RfidAChip=Integer,RfidBaudRate=Integer,RfidPasswordType=Integer,\
+RfidTerminalType=Integer,RfidAuthenticationProcedureType=Integer,RfidCertificateOrigin=Integer,RfidCertificateType=Integer" \
 \
 && docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "$DOCS_DEFINITION_FOLDER:/definitions" \
 openapitools/openapi-generator-cli:v5.0.0-beta2 generate \
